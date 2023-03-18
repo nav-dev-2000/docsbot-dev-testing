1f5dc40718824bad6c618698603b45413edade5e
Aaron Edwards <aaron@uglyrobot.com>
2023-03-17 22:54:41 -0500

    Add link to openai

--- src/pages/app/api.jsx
+++ src/pages/app/api.jsx
@@ -1,8 +1,7 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import { useEffect, useState, useRef } from 'react'
-import {
-  PencilIcon,
-} from '@heroicons/react/24/outline'
+import { PencilIcon } from '@heroicons/react/24/outline'
 import { getAuthorizedUserCurrentTeam } from '@/middleware/getAuthorizedUserCurrentTeam'
 import DashboardWrap from '@/components/DashboardWrap'
 import Alert from '@/components/Alert'
@@ -30,7 +29,16 @@ function Api({ team }) {
         <h3 className="text-2xl font-bold">OpenAI API Key</h3>
         <p className="text-md mt-2 text-justify text-gray-800">
           You can update your API key here. You must have a valid API key with billing enabled in
-          your OpenAI account for DocsBot to function.
+          your OpenAI account for DocsBot to function.{' '}
+          <Link
+            type="button"
+            className="inline-flex w-full justify-center rounded-md border border-transparent bg-cyan-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:ring-offset-2 sm:text-sm"
+            href="https://beta.openai.com/account/api-keys"
+            target="_blank"
+          >
+            Get my OpenAI key
+          </Link>
+          .
         </p>
         <div className="mt-4 flex justify-between">
           <div className="mt-4 flex items-center justify-start">
@@ -51,7 +59,7 @@ function Api({ team }) {
         </div>
       </div>
 
-      <div className="rounded-lg bg-white p-8 shadow mt-8">
+      <div className="mt-8 rounded-lg bg-white p-8 shadow">
         <h3 className="text-2xl font-bold">API Documentation</h3>
         <APIDocs team={team} />
       </div>
