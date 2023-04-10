ddd9206cc1a6a88cbc354d2618027b80a243d9d3
CPunch <sethtstubbs@gmail.com>
2023-04-10 15:19:14 -0500

    send responses

--- src/pages/api/teams/index.js
+++ src/pages/api/teams/index.js
@@ -29,36 +29,43 @@ export default async function handler(req, res) {
     const { team } = check
     const { removeUserId, removeUserEmail } = req.body
 
-    await firestore.runTransaction(async (transaction) => {
-      // remove member from teamRoles
-      if (removeUserId !== null) {
-        const teamRef = firestore.collection('teams').doc(team.id)
-        const teamDoc = await transaction.get(teamRef)
-        const isAdded = teamDoc.data().roles[removeUserId]
-  
-        if (isAdded === undefined) {
-          throw new Error('User is not part of this team!')
-        }
-  
-        // remove from team roles
-        let newRoles = teamDoc.data().roles
-        delete newRoles[removeUserId]
-        await transaction.update(teamRef, {
-          roles: newRoles
-        })
-  
-        // set the user's currentTeam to their default team
-        await assignDefaultTeamTransaction(transaction, removeUserId, 'User')
-      } else if (removeUserEmail !== null) { // remove invite
-        const invites = await getInvitesFromEmailAndTeamIdTransaction(transaction, removeUserEmail, team.id)
-        if (invites.length <= 0) {
-          throw new Error('Email was not invited!')
-        }
-        const invite = invites[0];
+    try {
+      await firestore.runTransaction(async (transaction) => {
+        // remove member from teamRoles
+        if (removeUserId !== null) {
+          const teamRef = firestore.collection('teams').doc(team.id)
+          const teamDoc = await transaction.get(teamRef)
+          const isAdded = teamDoc.data().roles[removeUserId]
 
-        await transaction.delete(firestore.collection('invites').doc(invite.id))
-      }
-    })
+          if (isAdded === undefined) {
+            throw new Error('User is not part of this team!')
+          }
+
+          // remove from team roles
+          let newRoles = teamDoc.data().roles
+          delete newRoles[removeUserId]
+          await transaction.update(teamRef, {
+            roles: newRoles
+          })
+
+          // set the user's currentTeam to their default team
+          await assignDefaultTeamTransaction(transaction, removeUserId, 'User')
+        } else if (removeUserEmail !== null) { // remove invite
+          const invites = await getInvitesFromEmailAndTeamIdTransaction(transaction, removeUserEmail, team.id)
+          if (invites.length <= 0) {
+            throw new Error('Email was not invited!')
+          }
+          const invite = invites[0];
+
+          await transaction.delete(firestore.collection('invites').doc(invite.id))
+        }
+      })
+
+      return res.status(200).send({ message: `Removed user successfully`})
+    } catch (error) {
+      console.log(err)
+      return res.status(500).json({ message: err?.message })
+    }
   } else {
     res.status(400).send({ message: 'Invalid HTTP method' })
   }
