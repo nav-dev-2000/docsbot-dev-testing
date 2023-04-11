169c185251ca8d66a259b0f9d0388d6c6c7c70a3
CPunch <sethtstubbs@gmail.com>
2023-04-11 14:16:44 -0500

    improved colors

--- src/pages/_error.jsx
+++ src/pages/_error.jsx
@@ -1,12 +1,12 @@
 const Error = ({ statusCode, statusMessage }) => {
   return (
-    <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <main class="grid min-h-full place-items-center bg-gray-900 px-6 py-24 sm:py-32 lg:px-8">
       <div class="text-center">
-        <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">{statusCode}</h1>
-        <p class="mt-6 text-base leading-7 text-gray-600">{statusMessage}</p>
+        <h1 class="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">{statusCode}</h1>
+        <p class="mt-6 text-base leading-7 text-gray-300">{statusMessage}</p>
         <div class="mt-10 flex items-center justify-center gap-x-6">
-          <a href="/app" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go back home</a>
-          <a href="#" onClick={() => {DocsBotAI.open();}} class="text-sm font-semibold text-gray-900">Contact support <span aria-hidden="true">&rarr;</span></a>
+          <a href="/app" class="rounded-md bg-gradient-to-r from-teal-500 to-cyan-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:from-teal-600 hover:to-cyan-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go back home</a>
+          <a href="#" onClick={() => {DocsBotAI.open();}} class="text-sm font-semibold text-white">Contact support <span aria-hidden="true">&rarr;</span></a>
         </div>
       </div>
     </main>
