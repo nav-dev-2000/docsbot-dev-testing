47766c46f0bc5de4fdda81902f4b1eba4f91552c
Aaron Edwards <aaron@uglyrobot.com>
2023-04-11 16:54:24 -0500

    fix classnames

--- src/pages/_error.jsx
+++ src/pages/_error.jsx
@@ -1,12 +1,12 @@
 const Error = ({ statusCode, statusMessage }) => {
   return (
-    <main classname="grid min-h-full place-items-center bg-gray-900 px-6 py-24 sm:py-32 lg:px-8">
-      <div classname="text-center">
-        <h1 classname="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">{statusCode}</h1>
-        <p classname="mt-6 text-base leading-7 text-gray-300">{statusMessage}</p>
-        <div classname="mt-10 flex items-center justify-center gap-x-6">
-          <a href="/" classname="rounded-md bg-gradient-to-r from-teal-500 to-cyan-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:from-teal-600 hover:to-cyan-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600">Go back home</a>
-          <a href="#" onClick={() => {DocsBotAI.open();}} classname="text-sm font-semibold text-white">Contact support <span aria-hidden="true">&rarr;</span></a>
+    <main className="grid min-h-full place-items-center bg-gray-900 px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">{statusCode}</h1>
+        <p className="mt-6 text-base leading-7 text-gray-300">{statusMessage}</p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <a href="/" className="rounded-md bg-gradient-to-r from-teal-500 to-cyan-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:from-teal-600 hover:to-cyan-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600">Go back home</a>
+          <a href="#" onClick={() => {DocsBotAI.open();}} className="text-sm font-semibold text-white">Contact support <span aria-hidden="true">&rarr;</span></a>
         </div>
       </div>
     </main>
