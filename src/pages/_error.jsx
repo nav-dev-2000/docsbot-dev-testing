718f5418a1bd014d079ab33ce79cb4a2874a7951
CPunch <sethtstubbs@gmail.com>
2023-04-11 14:04:18 -0500

    error page looks much nicer

--- src/pages/_error.jsx
+++ src/pages/_error.jsx
@@ -1,22 +1,30 @@
-function Error({ statusCode }) {
+const Error = ({ statusCode, statusMessage }) => {
   return (
-    <section>
-      <div className="min-h-screen px-4 mx-auto max-w-7xl px-12 py-24">
-        <div className="flex flex-col w-full text-center">
-          <p>
-            {statusCode
-              ? `An error ${statusCode} occurred while processing your request`
-              : 'Whoops! Something went wrong...'}
-          </p>
+    <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div class="text-center">
+        <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">{statusCode}</h1>
+        <p class="mt-6 text-base leading-7 text-gray-600">{statusMessage}</p>
+        <div class="mt-10 flex items-center justify-center gap-x-6">
+          <a href="/app" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go back home</a>
+          <a href="#" onClick={() => {DocsBotAI.open();}} class="text-sm font-semibold text-gray-900">Contact support <span aria-hidden="true">&rarr;</span></a>
         </div>
       </div>
-    </section>
+    </main>
   )
+}
+
+const getStatusMessage = (statusCode) => {
+  switch (statusCode) {
+    case 404: return "Sorry, but we couldn't find the page you were looking for.";
+    case 500: return "Sorry, but we encountered an internal server error while processing your request.";
+    default: return "Whoops! An unknown error occurred!";
+  }
 }
 
 Error.getInitialProps = ({ res, err }) => {
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404
-  return { statusCode }
+  const statusMessage = err ? err.statusMessage : getStatusMessage(statusCode)
+  return { statusCode, statusMessage }
 }
 
 export default Error
