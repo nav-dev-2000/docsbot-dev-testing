c96e0e4dcb3574c8d5341caa56655f6e59df9217
CPunch <sethtstubbs@gmail.com>
2023-04-13 17:30:34 -0500

    better pseudo-random number generator

--- src/utils/fakeUsers.js
+++ src/utils/fakeUsers.js
@@ -3,14 +3,22 @@ import {adjectives, nouns} from '@/utils/wordList'
 const getFakeUserByIp = (ip) => {
   const seeder = (s) => { // generate pseudo-random numbers based off a unique string
     let seed = 0
-    for (let i = 0; i < s.length; i++) {
-      const chr = s.charAt(i)
-      seed = ((seed << 5) - seed) + chr;
-      seed |= 0
+    let h1 = 0xdeadbeef, h2 = 0x41c6ce57;
+    for(let i = 0, ch; i < s.length; i++) {
+        ch = s.charCodeAt(i);
+        h1 = Math.imul(h1 ^ ch, 2654435761);
+        h2 = Math.imul(h2 ^ ch, 1597334677);
     }
+    h1  = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+    h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+    h2  = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+    h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+  
+    seed = 4294967296 * (2097151 & h2) + (h1 >>> 0);
 
     return () => {
-      seed = Math.sin(seed) * 10000; return seed - Math.floor(seed);
+      seed = (2364617249 * seed + 1261244998)
+      return Math.sin(seed)
     }
   }
 
