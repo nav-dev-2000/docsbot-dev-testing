89b993e24cfa35eadb1df4f54364b3eaa168bb02
CPunch <sethtstubbs@gmail.com>
2023-05-11 10:55:10 -0500

    started export-log endpoint

---
+++ src/pages/api/teams/[teamId]/bots/[botId]/export-log.js
@@ -0,0 +1,57 @@
+import { getBot } from '@/lib/dbQueries';
+import { stripePlan } from '@/utils/helpers';
+
+// this handler will export the question log of a bot to a csv file
+const handler = async (req, res) => {
+  configureFirebaseApp()
+  const firestore = getFirestore()
+
+  //check if user has access to team
+  let check = null
+  try {
+    check = await userTeamCheck(req, res)
+  } catch (error) {
+    return res.status(403).json({ message: error?.message })
+  }
+  const { userId, team } = check
+  const { botId } = req.query
+
+  if (req.method === 'GET') {
+    // grab bot
+    const bot = await getBot(team.id, botId)
+    if (!bot) {
+      return res.status(404).json({ message: 'Bot not found' })
+    }
+
+    // grab questions
+    const planLimit = stripePlan(team).logLimit
+    const questions = await firestore
+      .collection('teams')
+      .doc(team.id)
+      .collection('bots')
+      .doc(botId)
+      .collection('questions')
+      .orderBy('createdAt', 'desc')
+      .limit(planLimit)
+      .get()
+
+    // respond with csv file
+    res.setHeader('Content-Type', 'text/csv')
+    res.setHeader('Content-Disposition', 'attachment; filename=\"' + botId + '-questions.csv\"')
+    res.setHeader('Cache-Control', 'no-cache')
+    res.setHeader('Pragma', 'no-cache')
+
+    // write questions to csv file
+    questions.forEach((doc) => {
+      const question = { id: doc.id, ...doc.data(), alias: doc.data().ip ? getFakeUserByIp(doc.data().ip) : 'unknown-user'}
+      res.write(`${question.alias},${question.createdAt},${question.question},${question.answer}\n`)
+    })
+
+    res.status(200).end()
+  } else {
+    res.setHeader('Allow', ['GET'])
+    res.status(405).end(`Method ${req.method} Not Allowed`)
+  }
+}
+
+export default handler
