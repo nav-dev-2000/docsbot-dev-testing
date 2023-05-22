eb332f69ce9e4c58152e07b3d0df5aad0a204e3c
CPunch <sethtstubbs@gmail.com>
2023-05-22 10:54:06 -0500

    added blacklist for aliases
    - set environment variable BLACKLISTED_ALIASES to a JSON array of strings of the aliases to be blocked.
    - aliases are generated until a non-blacklisted alias is generated.

--- src/utils/fakeUsers.js
+++ src/utils/fakeUsers.js
@@ -3,8 +3,15 @@ import random from 'random'
 import seedrandom from 'seedrandom'
 
 const getFakeUserByIp = (ip) => {
+  const blacklistedAliases = JSON.parse(process.env.BLACKLISTED_ALIASES);
+  let alias;
+
   random.use(seedrandom(ip))
-  return random.choice(adjectives) + '-' + random.choice(nouns)
+  do { // keep generating aliases until we get one that isn't blacklisted
+    alias = random.choice(adjectives) + '-' + random.choice(nouns);
+  } while (blacklistedAliases.includes(alias))
+
+  return alias
 }
 
 export default getFakeUserByIp
