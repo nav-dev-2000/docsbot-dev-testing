fce59b2a7b0b9c083c5050aeecf0b4b31361be30
CPunch <sethtstubbs@gmail.com>
2023-06-14 14:26:41 -0500

    started download endpoint

---
+++ src/pages/api/teams/[teamId]/bots/[botId]/sources/[sourceId]/download.js
@@ -0,0 +1,49 @@
+import { configureFirebaseApp } from '@/config/firebase-server.config'
+import { getFirestore, FieldValue } from 'firebase-admin/firestore'
+import { getBot, getSource } from '@/lib/dbQueries'
+import { QueueSourceRegest } from '@/lib/service'
+import { checkSourceScheduledFromInterval } from '@/utils/helpers'
+import { canSourceTypeDownload } from '@/constants/sourceTypes.constants'
+import userTeamCheck from '@/lib/userTeamCheck'
+
+export default async function handler(req, res) {
+  configureFirebaseApp()
+  const firestore = getFirestore()
+
+  //check if user has access to team
+  let check = null
+  try {
+    check = await userTeamCheck(req, res)
+  } catch (error) {
+    return res.status(403).json({ message: error?.message })
+  }
+  const { userId, team } = check
+  const { botId, sourceId } = req.query
+
+  let bot = null
+  let source = null
+  try {
+    bot = await getBot(team.id, botId)
+    if (!bot) {
+      // doc.data() will be undefined in this case
+      return res.status(404).json({ message: "botId doesn't exist." })
+    }
+    source = await getSource(team, bot, sourceId)
+    if (!source) {
+      return res.status(404).json({ message: "sourceId doesn't exist." })
+    }
+  } catch (error) {
+    console.warn('Error getting document:', error)
+    return res.status(500).json({ message: error?.message })
+  }
+
+  if (!canSourceTypeDownload(source.type)) {
+    return res.status(400).json({ message: `Source type ${source.type} cannot be downloaded`})
+  }
+
+  if (req.method === 'GET') {
+    
+  } else {
+    return res.status(400).json({ message: 'Invalid HTTP method' })
+  }
+}
