7280df6357dfc4b7be0720f0ed41a788767c12de
CPunch <sethtstubbs@gmail.com>
2023-06-14 16:10:51 -0500

    completed download endpoint

--- src/pages/api/teams/[teamId]/bots/[botId]/sources/[sourceId]/download.js
+++ src/pages/api/teams/[teamId]/bots/[botId]/sources/[sourceId]/download.js
@@ -1,14 +1,15 @@
 import { configureFirebaseApp } from '@/config/firebase-server.config'
-import { getFirestore, FieldValue } from 'firebase-admin/firestore'
+import { firebaseConfig } from '@/config/firebase-ui.config'
+import { getFirestore } from 'firebase-admin/firestore'
+import { getStorage } from 'firebase-admin/storage'
 import { getBot, getSource } from '@/lib/dbQueries'
-import { QueueSourceRegest } from '@/lib/service'
-import { checkSourceScheduledFromInterval } from '@/utils/helpers'
 import { canSourceTypeDownload } from '@/constants/sourceTypes.constants'
 import userTeamCheck from '@/lib/userTeamCheck'
 
 export default async function handler(req, res) {
   configureFirebaseApp()
   const firestore = getFirestore()
+  const bucket = getStorage().bucket(`gs://${firebaseConfig.storageBucket}`)
 
   //check if user has access to team
   let check = null
@@ -37,12 +38,20 @@ export default async function handler(req, res) {
     return res.status(500).json({ message: error?.message })
   }
 
-  if (!canSourceTypeDownload(source.type)) {
+  if (!canSourceTypeDownload(source.type) || source?.file === undefined) {
     return res.status(400).json({ message: `Source type ${source.type} cannot be downloaded`})
   }
 
   if (req.method === 'GET') {
-    
+    const file = bucket.file(source.file)
+
+    // sign url for 7 days
+    const url = (await file.getSignedUrl({
+      action: 'read',
+      expires: Date.now() + 1000 * 60 * 60 * 24 * 7 // 7 days
+    }))[0];
+
+    return res.status(200).json({ url })
   } else {
     return res.status(400).json({ message: 'Invalid HTTP method' })
   }
