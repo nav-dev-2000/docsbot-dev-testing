b491e156db72b2d79a4153d232fdc335658c4e57
Aaron Edwards <aaron@uglyrobot.com>
2023-09-20 23:06:45 -0500

    Fix wrong cloud funciton timeout

--- src/lib/dbQueries.js
+++ src/lib/dbQueries.js
@@ -115,7 +115,7 @@ export async function getSources(teamId, bot, resultLimit = 1000, ascending = fa
   querySnapshot.forEach((doc) => {
     let source = { id: doc.id, ...doc.data() }
     //if createdAt is more than X hour ago and indexing is not complete, set error
-    const expireHours = ['urls', 'sitemap'].includes(source.type) ? 4 : 0.5 //APIFY has 4hr timeout, cloud funtions has 30mins
+    const expireHours = ['urls', 'sitemap'].includes(source.type) ? 6 : 1 //APIFY has 6hr timeout, cloud funtions has 60mins
     if (
       ['indexing', 'pending', 'processing'].includes(source.status) &&
       source.createdAt.toDate() < new Date(Date.now() - expireHours * 60 * 60 * 1000)
