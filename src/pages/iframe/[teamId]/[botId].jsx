4b863e5d5ed90e271facfd36f6394a6915d9909b
Aaron Edwards <aaron@uglyrobot.com>
2023-09-25 18:18:06 -0500

    Fix iframe border hidden

--- src/pages/iframe/[teamId]/[botId].jsx
+++ src/pages/iframe/[teamId]/[botId].jsx
@@ -32,7 +32,7 @@ export function ChatPage({ team, bot }) {
             {`window.DocsBotAI=window.DocsBotAI||{},DocsBotAI.init=function(c){return new Promise(function(e,o){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://widget.docsbot.ai/chat.js";const n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n),t.addEventListener("load",function(){window.DocsBotAI.mount({id:c.id,supportCallback:c.supportCallback,identify:c.identify,options:c.options,signature:c.signature});let t;t=function(n){return new Promise(function(e){if(document.querySelector(n))return e(document.querySelector(n));const o=new MutationObserver(function(t){document.querySelector(n)&&(e(document.querySelector(n)),o.disconnect())});o.observe(document.body,{childList:!0,subtree:!0})})},t&&t("#docsbotai-root").then(e).catch(o)}),t.addEventListener("error",function(t){o(t.message)})})};
               DocsBotAI.init({id: "${team.id}/${bot.id}"});`}
           </Script>
-          <div id="docsbot-widget-embed" className="h-full w-full bg-transparent"></div>
+          <div id="docsbot-widget-embed" className="h-full w-full bg-transparent py-0.5"></div>
         </>
       )}
     </>
