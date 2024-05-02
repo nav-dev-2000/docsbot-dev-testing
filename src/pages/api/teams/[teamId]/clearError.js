ea437c3f94e876e841bccb3ee6b7b6b4b92fdec6
CPunch <sethtstubbs@gmail.com>
2024-05-02 13:17:02 -0500

    added /api/teams/[teamId]/clearError endpoint

---
+++ src/pages/api/teams/[teamId]/clearError.js
@@ -0,0 +1,30 @@
+import userTeamCheck from '@/lib/userTeamCheck'
+import { getTeam } from '@/lib/dbQueries'
+import { clearLastError } from '@/lib/apiFunctions'
+
+export default async function handler(req, res) {
+  let check = null
+  try {
+    check = await userTeamCheck(req, res)
+  } catch (error) {
+    return res.status(403).json({ message: error?.message })
+  }
+  const { userId, team } = check
+
+  if (req.method === 'POST') {
+    // sanity check user permissions
+    // for now, all team members are allowed to clear errors
+    // if (!canUserModifyTeam(team, userId) && !isSuperAdmin(userId)) {
+    //   return res.status(403).json({ message: 'Unauthorized action; please contact your team owner.'})
+    // }
+
+    try {
+      await clearLastError(team)
+      return res.json(await getTeam(team.id))
+    } catch (error) {
+      return res.status(500).json({ message: error?.message })
+    }
+  } else {
+    return res.status(400).json({ message: 'Invalid HTTP method' })
+  }
+}
