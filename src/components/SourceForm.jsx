5326dd92ac10bc1ca5c2f9dbda645a61814cacf9
CPunch <sethtstubbs@gmail.com>
2024-05-23 14:02:27 -0500

    fix: carbon for zendesk

--- src/components/SourceForm.jsx
+++ src/components/SourceForm.jsx
@@ -111,7 +111,7 @@ export default function SourceForm({ team, bot, sources, setSources, setOpenSour
 
   const carbonOnSuccess = async (response) => {
     console.log('OnSuccess callback called!', response)
-    if (!response.data || !response.data?.files || !response.data?.files?.length) {
+    if (!response.data || !response.data?.data_source_external_id) {
       return
     }
 
