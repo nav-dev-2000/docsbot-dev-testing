8ce17777f536e879ceaac5c6c7e0ba8d0cdc7ee2
CPunch <sethtstubbs@gmail.com>
2024-06-13 13:22:48 -0500

    ModalSource: show refresh for carbon types if they're not schedule-able

--- src/components/ModalSource.jsx
+++ src/components/ModalSource.jsx
@@ -18,7 +18,7 @@ import LoadingSpinner from '@/components/LoadingSpinner'
 import BadgeStatusSource from '@/components/BadgeStatusSource'
 import ModalCheckout from '@/components/ModalCheckout'
 import ScheduleSelect from '@/components/ScheduleSelect'
-import { canSourceTypeSchedule, canSourceTypeDownload } from '@/constants/sourceTypes.constants'
+import { canSourceTypeSchedule, canSourceTypeDownload, isCarbonSourceType } from '@/constants/sourceTypes.constants'
 import QAForm from '@/components/QAForm'
 import Link from 'next/link'
 import { auth } from '@/config/firebase-ui.config'
@@ -552,6 +552,20 @@ export default function ModalSource({
                               Save
                             </button>
                           </div>
+                        )}
+                        {!showInterval && isCarbonSourceType(source?.type) && (
+                          <div className="flex flex-shrink-0 items-end justify-end">
+                          <button
+                            type="button"
+                            className={"inline-flex items-center justify-center rounded-md border px-4 py-2 text-sm font-medium text-gray-600 shadow-sm disabled:opacity-75" +
+                              (canModify ? " hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:ring-offset-2 border-gray-300 bg-white" : " border-gray-200 bg-gray-300 cursor-not-allowed")}
+                            onClick={refreshSource}
+                            disabled={submitting || locked !== null || !canModify}
+                          >
+                            <ArrowPathIcon className="mr-2 h-5 w-5" aria-hidden="true" />
+                            Refresh
+                          </button>
+                        </div>
                         )}
                       </div>
                     )}
