da4d8191666fbdacb885b8e471569eaf1afa6f1f
Aaron Edwards <aaron@uglyrobot.com>
2024-10-15 18:55:08 -0500

    Fix prompt modal style

--- src/components/ModalPrompt.jsx
+++ src/components/ModalPrompt.jsx
@@ -1,6 +1,7 @@
 import { Fragment, useEffect, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { CommandLineIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import clsx from 'clsx'
 import LoadingSpinner from '@/components/LoadingSpinner'
 import ModalCheckout from '@/components/ModalCheckout'
 import { stripePlan, isSuperAdmin } from '@/utils/helpers'
@@ -9,6 +10,7 @@ import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth } from '@/config/firebase-ui.config'
 import { canUserEditBot } from '@/utils/function.utils'
 import Tooltip from '@/components/Tooltip'
+
 
 export default function ModalPrompt({ team, integrations, bot }) {
   const [open, setOpen] = useState(false)
@@ -19,12 +21,18 @@ export default function ModalPrompt({ team, integrations, bot }) {
   const [showUpgrade, setShowUpgrade] = useState(false)
   const [user] = useAuthState(auth)
   const [canModify, setModify] = useState(false)
+  const [activeTab, setActiveTab] = useState('regular')
   const helpScoutIntegration = integrations.find((i) => i.id === 'helpscout')
 
   useEffect(() => {
     if (!team || !user) return
     setModify(canUserEditBot(team, user.uid))
   }, [team, user])
+
+  const tabs = [
+    { name: 'Regular Prompt', id: 'regular', current: activeTab === 'regular' },
+    { name: 'Help Scout Prompt', id: 'helpscout', current: activeTab === 'helpscout', disabled: !helpScoutIntegration },
+  ]
 
   async function updatePrompt() {
     setErrorText('')
@@ -108,7 +116,7 @@ export default function ModalPrompt({ team, integrations, bot }) {
                 leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
               >
-                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-6xl sm:p-6">
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-7xl sm:p-6">
                   <form
                     onSubmit={(e) => {
                       e.preventDefault()
@@ -138,8 +146,8 @@ export default function ModalPrompt({ team, integrations, bot }) {
                         )}
                       </Dialog.Title>
                       <Alert title={errorText} type="error" />
-                      <div className="grid grid-cols-1 gap-x-8 lg:grid-cols-2">
-                        <div className="">
+                      <div className="grid grid-cols-1 gap-x-8 lg:grid-cols-3">
+                        <div className="lg:col-span-1">
                           <p className="text-md text-gray-700">
                             Add some custom instructions to your prompt to
                             adjust your bot's answer output to your specific use
@@ -186,45 +194,69 @@ export default function ModalPrompt({ team, integrations, bot }) {
                             </li>
                           </ul>
                         </div>
-                        <div className="flex">
-                          <div className="mr-2 mt-4 h-full w-full lg:mt-0">
-                            <label
-                              htmlFor="prompt"
-                              className="block text-left text-sm font-medium text-gray-700"
-                            >
-                              Custom Prompt
-                            </label>
-                            <div className="mt-1 h-full">
-                              <textarea
-                                id="prompt"
-                                className="block h-full w-full rounded-md border-gray-300 shadow-sm focus:border-cyan-500 focus:ring-cyan-500 sm:text-sm"
-                                placeholder="Enter any custom prompt here..."
-                                value={prompt}
-                                onChange={(e) => setPrompt(e.target.value)}
-                                disabled={isUpdating || !canModify}
-                              />
-                            </div>
+                        <div className="flex flex-col lg:col-span-2">
+                          <div className="border-b border-gray-200">
+                            <nav className="-mb-px flex space-x-8" aria-label="Tabs">
+                              {tabs.map((tab) => (
+                                <a
+                                  key={tab.name}
+                                  onClick={() => !tab.disabled && setActiveTab(tab.id)}
+                                  className={clsx(
+                                    tab.current
+                                      ? 'border-cyan-500 text-cyan-600'
+                                      : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
+                                    'whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium cursor-pointer',
+                                    tab.disabled && 'opacity-50 cursor-not-allowed'
+                                  )}
+                                  aria-current={tab.current ? 'page' : undefined}
+                                >
+                                  {tab.name}
+                                </a>
+                              ))}
+                            </nav>
                           </div>
-                          {helpScoutIntegration && (
-                            <div className="mt-4 h-full w-full lg:mt-0">
-                              <label
-                                htmlFor="helpscoutPrompt"
-                                className="block text-left text-sm font-medium text-gray-700"
-                              >
-                                Custom Help Scout Prompt
-                              </label>
-                              <div className="mt-1 h-full">
-                                <textarea
-                                  id="helpscoutPrompt"
-                                  className="block h-full w-full rounded-md border-gray-300 shadow-sm focus:border-cyan-500 focus:ring-cyan-500 sm:text-sm"
-                                  placeholder="Enter any custom prompt here, this will be used for helpscout responses."
-                                  value={hsPrompt}
-                                  onChange={(e) => setHSPrompt(e.target.value)}
-                                  disabled={isUpdating || !canModify}
-                                />
+                          <div className="-mt-1 flex-grow">
+                            {activeTab === 'regular' && (
+                              <div className="h-full">
+                                <label
+                                  htmlFor="prompt"
+                                  className="sr-only"
+                                >
+                                  Custom Prompt
+                                </label>
+                                <div className="mt-4 h-full">
+                                  <textarea
+                                    id="prompt"
+                                    className="block h-full w-full rounded-md border-gray-300 shadow-sm focus:border-cyan-500 focus:ring-cyan-500 sm:text-sm min-h-96"
+                                    placeholder="Enter any custom prompt here..."
+                                    value={prompt}
+                                    onChange={(e) => setPrompt(e.target.value)}
+                                    disabled={isUpdating || !canModify}
+                                  />
+                                </div>
                               </div>
-                            </div>
-                          )}
+                            )}
+                            {activeTab === 'helpscout' && helpScoutIntegration && (
+                              <div className="h-full">
+                                <label
+                                  htmlFor="helpscoutPrompt"
+                                  className="block text-left text-sm font-medium text-gray-700"
+                                >
+                                  Custom Help Scout Prompt
+                                </label>
+                                <div className="mt-1 h-full">
+                                  <textarea
+                                    id="helpscoutPrompt"
+                                    className="block h-full w-full rounded-md border-gray-300 shadow-sm focus:border-cyan-500 focus:ring-cyan-500 sm:text-sm"
+                                    placeholder="Enter any custom prompt here, this will be used for helpscout responses."
+                                    value={hsPrompt}
+                                    onChange={(e) => setHSPrompt(e.target.value)}
+                                    disabled={isUpdating || !canModify}
+                                  />
+                                </div>
+                              </div>
+                            )}
+                          </div>
                         </div>
                       </div>
                       <div className="mt-14 flex justify-end sm:mt-12">
