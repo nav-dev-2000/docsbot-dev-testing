48af3922cec98467dc0a259feadd4ebf3220a8c5
Aaron Edwards <aaron@uglyrobot.com>
2024-11-13 16:23:25 -0600

    Redo hero on homepage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,7 +14,7 @@ import {
   LifebuoyIcon,
   ChatBubbleLeftEllipsisIcon,
 } from '@heroicons/react/20/solid'
-import qaHeader from '@/images/docsbot-header.svg'
+import qaHeader from '@/images/header-screenshot.png'
 import demoScreenshot from '@/images/demo-screenshot.png'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -110,71 +110,110 @@ export default function Home() {
       <Head />
       <div className="bg-white">
         <div className="relative overflow-hidden">
-          <Header />
+          <Header transparent />
           <main>
-            <div className="bg-gray-900 pt-4 sm:pt-10 lg:overflow-hidden lg:pb-14 lg:pt-2">
-              <div className="mx-auto max-w-7xl lg:px-8">
-                <div className="lg:grid lg:grid-cols-2 lg:gap-8">
-                  <div className="mx-auto max-w-md px-6 sm:max-w-2xl sm:text-center lg:flex lg:items-center lg:px-0 lg:text-left">
-                    <div className="lg:py-24">
-                      <div className="inline-flex items-center rounded-full bg-black p-1 pl-3 text-white hover:text-gray-200 sm:text-base lg:text-sm xl:text-base">
-                        <span className="pr-1 text-sm">Powered by</span>
+            <div className="-mt-24 bg-gray-900">
+              <div className="relative isolate overflow-hidden bg-gray-900">
+                <svg
+                  aria-hidden="true"
+                  className="absolute inset-0 -z-10 size-full stroke-white/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+                >
+                  <defs>
+                    <pattern
+                      x="50%"
+                      y={-1}
+                      id="983e3e4c-de6d-4c3f-8d64-b9761d1534cc"
+                      width={200}
+                      height={200}
+                      patternUnits="userSpaceOnUse"
+                    >
+                      <path d="M.5 200V.5H200" fill="none" />
+                    </pattern>
+                  </defs>
+                  <svg
+                    x="50%"
+                    y={-1}
+                    className="overflow-visible fill-gray-800/20"
+                  >
+                    <path
+                      d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
+                      strokeWidth={0}
+                    />
+                  </svg>
+                  <rect
+                    fill="url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)"
+                    width="100%"
+                    height="100%"
+                    strokeWidth={0}
+                  />
+                </svg>
+                <div
+                  aria-hidden="true"
+                  className="absolute left-[calc(50%-4rem)] top-10 -z-10 transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:left-48 lg:top-[calc(50%-30rem)] xl:left-[calc(50%-24rem)]"
+                >
+                  <div
+                    style={{
+                      clipPath:
+                        'polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)',
+                    }}
+                    className="aspect-[1108/632] w-[69.25rem] bg-gradient-to-r from-teal-500 to-cyan-600 opacity-20"
+                  />
+                </div>
+                <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40">
+                  <div className="mx-auto max-w-2xl shrink-0 lg:mx-0 lg:pt-8">
+                    <div className="mt-24 sm:mt-32 lg:mt-12">
+                      <div className="inline-flex items-center rounded-full bg-cyan-500/10 py-1 pl-3 pr-1 text-sm/6 font-semibold text-cyan-200 ring-1 ring-inset ring-cyan-500/20">
+                        <span className="pr-1">Powered by</span>
                         <span className="rounded-full bg-gradient-to-r from-teal-500 to-cyan-600 px-2 py-0.5 text-sm font-semibold leading-5 text-white">
                           ChatGPT & GPT-4o!
                         </span>
                       </div>
-                      <h1 className="mt-4 text-4xl font-bold tracking-tight text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
-                        <span className="block text-6xl sm:text-8xl">DocsBot AI</span>
-                        <span className="block bg-gradient-to-r from-teal-200 to-cyan-400 bg-clip-text pb-3 text-4xl text-transparent sm:pb-5 sm:text-5xl">
-                          ChatGPT for{' '}
-                          <span className="relative whitespace-nowrap text-white">
-                            <svg
-                              aria-hidden="true"
-                              viewBox="0 0 418 42"
-                              className="absolute left-0 top-2/3 h-[0.58em] w-full fill-cyan-400/70"
-                              preserveAspectRatio="none"
-                            >
-                              <path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z" />
-                            </svg>
-                            <span className="relative text-white">your</span>
-                          </span>{' '}
-                          docs
-                        </span>
-                      </h1>
-                      <p className="text-base text-gray-300 sm:text-xl lg:text-lg xl:text-xl">
-                        Get instant answers for you, your customers, or your team with AI powered
-                        chatbots trained with your content and documentation. Save money and improve
-                        the support experience for your customers, the productivity of your team,
-                        and AI copywriting with existing knowledge of your business!
-                      </p>
-                      <div className="mt-8">
-                        <Link
-                          href="/register"
-                          type="button"
-                          className="block w-full rounded-md bg-animation px-4 py-3 text-center font-bold text-white shadow hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2 focus:ring-offset-gray-900"
-                        >
-                          Create your own free DocsBot
-                        </Link>
-                      </div>
-                      <p className="mt-3 text-sm text-gray-300 sm:mt-4">
-                        Get started today, no credit card required!
+                    </div>
+                    <h1 className="mt-8 text-pretty text-5xl font-semibold tracking-tight text-white sm:text-7xl">
+                      <span className="block text-6xl leading-[0.8] tracking-tighter md:text-8xl md:leading-[0.8]">
+                        Instant AI Answers from your Docs
+                      </span>
+                      <span className="mt-2 block bg-gradient-to-r from-teal-200 to-cyan-400 bg-clip-text pb-3 text-3xl text-transparent sm:pb-5 sm:text-4xl">
+                        Custom ChatGPT for your business
+                      </span>
+                    </h1>
+                    <p className="mt-6 text-pretty text-lg font-medium text-gray-300 sm:text-xl/8">
+                      Get instant answers for you, your customers, or your team
+                      with custom AI ChatGPT chatbots trained with your content
+                      and documentation. Save money and improve the support
+                      experience for your customers, the productivity of your
+                      team, and generate new content with existing knowledge of
+                      your business!
+                    </p>
+                    <div className="mt-10">
+                      <Link
+                        href="/register"
+                        type="button"
+                        className="bg-animation block w-full cursor-pointer rounded-md px-4 py-3 text-center font-bold text-white shadow hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2 focus:ring-offset-gray-900"
+                      >
+                        Create your own free DocsBot
+                      </Link>
+                      <p className="mt-3 text-center text-sm text-gray-300 sm:mt-4">
+                        Transform your business with AI,{' '}
+                        <u>no credit card required</u>!
                       </p>
                     </div>
                   </div>
-                  <div className="-mb-16 mt-12 sm:-mb-48 lg:relative lg:m-0">
-                    <div className="mx-auto max-w-md px-6 sm:max-w-2xl lg:max-w-none lg:px-0">
+                  <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-16 lg:max-w-none lg:flex-none xl:ml-32">
+                    <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
                       <Image
-                        className="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+                        className="w-[76rem] rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10 ring-offset-4 ring-offset-gray-900/50"
                         src={qaHeader}
-                        alt="DocsBot Illustration"
+                        alt="DocsBot dashboard screenshot"
+                        priority
                       />
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="mx-auto max-w-7xl px-6 py-16 lg:px-8 lg:py-8 xl:py-4">
+              <div className="mx-auto max-w-7xl px-6 pb-16 lg:px-8 lg:pb-8 xl:pb-24">
                 <h2 className="mb-8 text-center text-lg font-semibold leading-8 text-white">
-                  Trusted by
+                  Trusted by more than 2,000 businesses!
                 </h2>
                 <TrustedBy />
               </div>
@@ -191,11 +230,13 @@ export default function Home() {
                     Train and deploy custom chatbots in minutes!
                   </p>
                   <p className="mx-auto mt-5 max-w-7xl text-xl text-gray-500">
-                    Are you tired of answering the same questions over and over again? Do you wish
-                    you had a way to automate your customer support and give your team more time to
-                    focus on other tasks? With DocsBot, you can do just that. We make it simple to
-                    build ChatGPT-powered bots that are trained with your content and documentation,
-                    so they can provide instant answers to your customers' most detailed questions.
+                    Are you tired of answering the same questions over and over
+                    again? Do you wish you had a way to automate your customer
+                    support and give your team more time to focus on other
+                    tasks? With DocsBot, you can do just that. We make it simple
+                    to build ChatGPT-powered bots that are trained with your
+                    content and documentation, so they can provide instant
+                    answers to your customers' most detailed questions.
                   </p>
                 </div>
               </div>
@@ -204,7 +245,7 @@ export default function Home() {
                   <Image
                     src={demoScreenshot}
                     alt="Application screenshot"
-                    className="mb-[-12%] rounded-xl shadow-2xl ring-1 ring-gray-900/10"
+                    className="mb-[-2%] rounded-xl shadow-2xl ring-1 ring-gray-900/10"
                   />
                   <div className="relative" aria-hidden="true">
                     <div className="absolute -inset-x-20 bottom-0 bg-gradient-to-t from-gray-50 pt-[7%]" />
@@ -239,10 +280,11 @@ export default function Home() {
                   Powerful integrations and API
                 </p>
                 <p className="mx-auto mt-5 max-w-7xl text-xl text-gray-500">
-                  Our intuitive interface makes it easy to index your documentation, blog posts, or
-                  any other content with just a few clicks. Then use our simple API and embeddable
-                  widgets to integrate your custom DocsBot into your website, WordPress, app/plugin,
-                  Slack, or anywhere else you want to use it!
+                  Our intuitive interface makes it easy to index your
+                  documentation, blog posts, or any other content with just a
+                  few clicks. Then use our simple API and embeddable widgets to
+                  integrate your custom DocsBot into your website, WordPress,
+                  app/plugin, Slack, or anywhere else you want to use it!
                 </p>
                 <div className="mt-12">
                   <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
@@ -252,13 +294,18 @@ export default function Home() {
                           <div className="-mt-6">
                             <div>
                               <span className="inline-flex items-center justify-center rounded-md bg-gradient-to-r from-teal-500 to-cyan-600 p-3 shadow-lg">
-                                <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
+                                <feature.icon
+                                  className="h-6 w-6 text-white"
+                                  aria-hidden="true"
+                                />
                               </span>
                             </div>
                             <h3 className="mt-8 text-lg font-medium tracking-tight text-gray-900">
                               {feature.name}
                             </h3>
-                            <p className="mt-5 text-base text-gray-500">{feature.description}</p>
+                            <p className="mt-5 text-base text-gray-500">
+                              {feature.description}
+                            </p>
                           </div>
                         </div>
                       </div>
