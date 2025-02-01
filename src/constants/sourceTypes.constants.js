2b3f9ec0fa4578ed10d9f48fdbfdf682bb8a472c
Aaron Edwards <aaron@uglyrobot.com>
2025-02-01 00:49:12 -0600

    Update document source type to support image file formats

--- src/constants/sourceTypes.constants.js
+++ src/constants/sourceTypes.constants.js
@@ -39,10 +39,11 @@ export const sourceTypes = [
   {
     id: 'document',
     title: 'Document',
-    description: 'Upload document files containing text (Text, Office, PDF, HTML, MD, EML, ZIP, Excel, CSV, TSV, etc)',
-    fullDescription: 'Upload a document file containing text to learn from. Some examples are product documentation, slide decks, research papers, employee handbooks, an ebook, or any content you can convert to a text file. We will parse the content, and add it to this bot. Currently we support adding .txt, .docx, .pptx, .md, .html, .pdf, .eml, .xlsx, .csv, and .tsv documents. For Excel, CSV, and TSV files, supports one table per sheet and the first row should contain thecolumn headers. Paid users can also add .zip files containing multiple documents!',
+    description: 'Upload document files containing text (Text, Office, PDF, HTML, MD, EML, ZIP, Excel, CSV, TSV, Images, etc)',
+    fullDescription: 'Upload a document file containing text to learn from. Some examples are product documentation, slide decks, research papers, employee handbooks, an ebook, or any content you can convert to a text file. We will parse the content, and add it to this bot. Currently we support adding .txt, .docx, .pptx, .md, .html, .pdf, .eml, .xlsx, .csv, and .tsv documents. For Excel, CSV, and TSV files, supports one table per sheet and the first row should contain the column headers. We also support .png, .jpeg, .webp, .heic, and .heif images for AI description and text extraction. Paid users can also add .zip files containing multiple documents!',
     icon: DocumentIcon,
     isPro: false,
+    isNew: true,
     coming: false,
     fieldUrl: 'optional',
     fieldTitle: 'required',
@@ -58,7 +59,12 @@ export const sourceTypes = [
       md: 'text/markdown',
       csv: 'text/csv',
       tsv: 'text/tab-separated-values',
-      xlsx: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+      xlsx: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      png: 'image/png',
+      jpeg: 'image/jpeg',
+      webp: 'image/webp',
+      heic: 'image/heic',
+      heif: 'image/heif'
     },
     fieldSchedule: false,
     fieldQA: false,
