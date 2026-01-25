2c0f176bc64b35478017734c2bbf252019a7dfab
Aaron Edwards <uglyrobot@users.noreply.github.com>
2026-01-24 18:03:50 -0600

    Disable vector DB maintenance flag (#452)

--- src/constants/maintenance.constants.js
+++ src/constants/maintenance.constants.js
@@ -1,4 +1,4 @@
-export const VECTOR_DB_MAINTENANCE_ENABLED = true
+export const VECTOR_DB_MAINTENANCE_ENABLED = false
 export const VECTOR_DB_MAINTENANCE_STATUS_PAGE = 'https://docsbot.instatus.com/'
 export const VECTOR_DB_MAINTENANCE_MESSAGE =
   'Vector database maintenance is in progress. Creating bots and managing sources is temporarily unavailable. Please try again in a few hours and check our status page for updates.'
